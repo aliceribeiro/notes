5bcba14e1cbbf5443aea59a726d47e4f225b84ea
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-03-02 23:57:14 -0300

    feat: :wrench: define tokens de fontSize e Colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,31 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      white: "#FFF",
+      black: {
+        700: "#2F2E41",
+        600: "#3F3D56",
+      },
+      gray: {
+        700: "#8A8E9B",
+        600: "#D9D9D9",
+        400: "#F2F2F2",
+      },
+      caramel: {
+        700: "#A96436",
+        600: "#BE835B",
+        "200-tp": "#DBBA87a3",
+      },
+    },
     extend: {
       fontFamily: {
         sans: "Inter, sans-serif",
