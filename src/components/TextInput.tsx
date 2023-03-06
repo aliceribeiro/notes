80b1bc30fb64606f816a41a3c1d7bd16cdacb3ae
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-03-05 21:53:59 -0300

    feat(comp): :sparkles: cria componente de Text Input

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,51 @@
+import { InputHTMLAttributes, ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
+
+
+
+export interface TextInputRootProps {
+  children: ReactNode;
+}
+
+function TextInputRoot(props: TextInputRootProps) {
+  return (
+    <div
+      className={clsx(
+        "flex items-center gap-3 h-12 py-3 px-3 rounded bg-gray-600 w-full focus-within:ring-2 ring-caramel-700"
+      )}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+TextInputRoot.displayName = "TextInput.Root";
+
+export interface TextInputIconProps {
+    children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+    return (
+        <Slot className="w-6 h-6 text-caramel-700">
+            {props.children}
+        </Slot>
+    )
+}
+
+TextInputIcon.displayName = "TextInput.Icon";
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+function TextInputInput(props: TextInputInputProps) {
+  return <input className="bg-transparent flex-1 text-black-600 placeholder:text-gray-700 outline-none" {...props} />;
+}
+
+TextInputInput.displayName = "TextInput.Input";
+
+export const TextInput = {
+    Root: TextInputRoot,
+    Input: TextInputInput,
+    Icon: TextInputIcon
+}
