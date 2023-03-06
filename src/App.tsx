3c048a45a0cff65d7320da293bfac70f80de2755
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-03-05 22:50:56 -0300

    chore: ajusta página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import "./styles/global.css";
 
 export function App() {
-  return <h1 className="font-bold text-5xl text-orange-900">Hello World</h1>;
+  return (
+    <>
+    <h1 className="font-bold text-2xl text-caramel-900">Hello World</h1>
+
+    <button className="bg-caramel-700 font-medium px-4 py-2 rounded text-white hover:bg-caramel-600">Enviar</button>
+    </>
+  );
 }
