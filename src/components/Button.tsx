7b23e5ba6220a05143e2b60392d86b0169821006
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-03-06 23:48:17 -0300

    feat(comp): :lipstick: adiciona propriedades de classe e nativas aos componentes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,27 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
       className={clsx(
-        "py-4 px-3 bg-caramel-700 rounded font-semibold text-gray-400 text-sm w-full transition-colors hover:bg-caramel-600 focus:ring-2 ring-caramel-700"
+        "py-3 px-4 bg-caramel-700 rounded font-semibold text-gray-400 text-sm w-full transition-colors hover:bg-caramel-600 focus:ring-2 ring-caramel-700",
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from "react";
 import { Check } from "phosphor-react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-600 rounded">
+    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-600 rounded" {...props}>
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-caramel-700" />
       </CheckboxPrimitive.Indicator>
