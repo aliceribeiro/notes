6bed75061b4a001089971e37f433cafd39546277
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-06-09 17:00:51 -0300

    feat: :sparkles: configura roteamento

---
+++ src/helpers/routes.ts
@@ -0,0 +1,9 @@
+enum BaseRoutes {
+  BASE = "/",
+  HOME = "/home"
+}
+
+export const ROUTES = {
+  BASE: BaseRoutes.BASE,
+  HOME: BaseRoutes.HOME
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ROUTES } from "./helpers/routes";
+
+const router = createBrowserRouter([
+  {
+    path: ROUTES.BASE,
+    element: <App />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
