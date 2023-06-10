c6d7360456a835a9077c7eadbcf1d2471e0ad0fb
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-06-10 19:49:43 -0300

    feat: :sparkles: cria tela de página de erro

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { App } from "./App";
+import { ErrorPage } from "./pages/ErrorPage/ErrorPage";
 import { ROUTES } from "./helpers/routes";
 
 const router = createBrowserRouter([
   {
     path: ROUTES.BASE,
-    element: <App />
+    element: <App />,
+    errorElement: <ErrorPage />
   }
 ]);
 
---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import { useNavigate, useRouteError } from "react-router-dom";
+import { Heading } from "../../components/Heading";
+import { Text } from "../../components/Text";
+import { Button } from "../../components/Button";
+import { ROUTES } from "../../helpers/routes";
+import NotFound from "../../assets/illustrations/NotFound";
+
+export function ErrorPage() {
+  const error = useRouteError();
+  console.log(error);
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-400">
+      <div className="w-auto flex flex-col items-center gap-4">
+        <Heading className="text-caramel-700">Oops!</Heading>
+        <NotFound />
+        <Text className="text-gray-700">Página não encontrada</Text>
+        <Button onClick={() => navigate(ROUTES.BASE)}>Voltar para o Login</Button>
+      </div>
+    </div>
+  );
+}
