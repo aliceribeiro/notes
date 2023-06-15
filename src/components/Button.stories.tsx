78cc8137591f19b07dc4d4c10403c112ad853c0d
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-06-14 22:26:42 -0300

    feat(comp): :sparkles: cria botão tipo link

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,13 +1,13 @@
-import { Meta, Story, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { Button, ButtonProps } from "./Button";
 
 export default {
   title: "Components/Button",
-  component: Button,
+  component: Button.Primary,
   args: {
     children: "Button text"
   },
-  argTypes: {},
+  argTypes: {}
 } as Meta<ButtonProps>;
 
 export const Default: StoryObj<ButtonProps> = {};
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,17 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactElement, ReactNode } from "react";
+import { Link } from "react-router-dom";
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+}
+
+export interface LinkButtonProps {
+  icon: ReactElement;
+  label: string;
+  navigateTo: string;
 }
 
 function ButtonRoot({ children, asChild, className, ...props }: ButtonProps) {
@@ -63,8 +70,20 @@ function ButtonSecondary({ children, asChild, className, ...props }: ButtonProps
 
 ButtonSecondary.displayName = "Button.Secondary";
 
+function LinkButton({ icon, label, navigateTo }: LinkButtonProps) {
+  return (
+    <div className="flex items-center gap-2 py-1 px-2 my-1 rounded transition-colors hover:bg-gray-600 focus:ring-2 ring-caramel-700 cursor-pointer">
+      <>{icon}</>
+      <Link to={navigateTo}>{label}</Link>
+    </div>
+  );
+}
+
+LinkButton.displayName = "Button.Link";
+
 export const Button = {
   Root: ButtonRoot,
   Primary: ButtonPrimary,
-  Secondary: ButtonSecondary
+  Secondary: ButtonSecondary,
+  Link: LinkButton
 };
