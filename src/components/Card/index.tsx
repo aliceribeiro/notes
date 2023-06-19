06bcdcb34c3f2011ac9e495e6b9298ffb32a6cb0
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-06-19 20:11:51 -0300

    feat(comp): :sparkles: cria componente de Card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,30 @@
+import { Tag, Trash } from "phosphor-react";
+import { Heading } from "../Heading";
+import { Text } from "../Text";
+
+export interface CardProps {
+  title: string;
+  date: string;
+  category: string;
+  text: string;
+  handleDelete: () => void;
+}
+
+export function Card({ title, date, category, text, handleDelete }: CardProps) {
+  return (
+    <div className="rounded bg-gray-600 p-4">
+      <div className="flex items-center justify-between text-caramel-700">
+        <Heading className="">{title}</Heading>
+        <Trash size={24} onClick={handleDelete} className="cursor-pointer" />
+      </div>
+      <Text size="sm" className="text-gray-700">
+        {date}
+      </Text>
+      <div className="flex items-center gap-2 py-2 text-caramel-700">
+        <Tag size={24} className="rotate-90" />
+        <Text>{category}</Text>
+      </div>
+      <Text className="text-black-600">{text}</Text>
+    </div>
+  );
+}
---
+++ src/stories/components/Card.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Card, CardProps } from "../../components/Card";
+
+export default {
+  title: "Components/Card",
+  component: Card,
+  args: {
+    title: "Título",
+    date: "24 de abril de 2023",
+    category: "Categoria",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    handleDelete: () => console.log("Cliquei na lixeira")
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+} as Meta<CardProps>;
+
+export const Default: StoryObj<CardProps> = {};
