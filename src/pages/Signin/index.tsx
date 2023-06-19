a2b8562a9d3f57da9c0a5bdf6c4abfb12edcda01
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-06-18 22:24:42 -0300

    feat(login): :sparkles: implementa autenticação do usuário por email e senha

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -7,22 +7,21 @@ import { TextInput } from "../../components/TextInput";
 import { Text } from "../../components/Text";
 import { Navigate } from "react-router-dom";
 import { ROUTES } from "../../helpers/routes";
-import LoginImage from "../../assets/illustrations/LoginImage";
-import { AuthGoogleContext } from "../../contexts/authGoogle";
+import { AuthContext } from "../../contexts/auth";
 import { Separator } from "../../components/Separator";
+import LoginImage from "../../assets/illustrations/LoginImage";
 
 const version = import.meta.env.VITE_VERSION;
 
 export function Signin() {
-  const navigate = useNavigate();
-  const { signinGoogle, signed } = useContext(AuthGoogleContext);
+  const { signin, signinGoogle, signed } = useContext(AuthContext);
 
-  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  async function handleSubmit(event: FormEvent) {
-    event.preventDefault();
-    navigate(ROUTES.HOME);
-    setIsUserSignedIn(true);
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    signin(email, password);
   }
 
   function loginGoogle() {
@@ -33,44 +32,42 @@ export function Signin() {
     return <Navigate to={ROUTES.HOME} />;
   } else {
     return (
-      <div className="w-screen h-screen flex">
-        <div className="w-1/2 h-screen flex flex-col justify-center items-center bg-caramel-200-tp">
+      <div className="flex h-screen w-screen">
+        <div className="flex h-screen w-1/2 flex-col items-center justify-center bg-caramel-200-tp">
           <LoginImage />
           <div className="text-caramel-200-tp">{version ? version : ""}</div>
         </div>
-        <div className="w-1/2 h-screen bg-gray-400 flex flex-col items-center justify-center">
+        <div className="flex h-screen w-1/2 flex-col items-center justify-center bg-gray-400">
           <header className="max-w-sm">
             <NotePencil size={42} className="text-caramel-700" />
             <Heading className="text-caramel-700">Olá 👋</Heading>
             <Text className="text-gray-700">Insira as informações que você cadastrou ao se registrar.</Text>
           </header>
 
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-stretch w-full max-w-sm mt-10">
-            {isUserSignedIn && <Text>Login realizado!</Text>}
-
+          <form onSubmit={handleSubmit} className="mt-10 flex w-full max-w-sm flex-col items-stretch gap-4">
             <label htmlFor="email">
-              <Text className="text-caramel-700 font-semibold">Endereço de e-mail</Text>
+              <Text className="font-semibold text-caramel-700">Endereço de e-mail</Text>
               <TextInput.Root>
                 <TextInput.Icon>
                   <EnvelopeSimple />
                 </TextInput.Icon>
 
-                <TextInput.Input type="email" id="email" placeholder="Digite seu e-mail" />
+                <TextInput.Input type="email" id="email" placeholder="Digite seu e-mail" onChange={(e) => setEmail(e.target.value)} />
               </TextInput.Root>
             </label>
 
             <label htmlFor="password">
-              <Text className="text-caramel-700 font-semibold">Senha</Text>
+              <Text className="font-semibold text-caramel-700">Senha</Text>
               <TextInput.Root>
                 <TextInput.Icon>
                   <Lock />
                 </TextInput.Icon>
 
-                <TextInput.Input type="password" id="password" placeholder="Digite sua senha" />
+                <TextInput.Input type="password" id="password" placeholder="Digite sua senha" onChange={(e) => setPassword(e.target.value)} />
               </TextInput.Root>
             </label>
 
-            <Text asChild size="sm" className="text-end mb-6">
+            <Text asChild size="sm" className="mb-6 text-end">
               <a href="#" className="text-caramel-700 underline hover:text-caramel-600">
                 Esqueceu a senha?
               </a>
@@ -79,9 +76,9 @@ export function Signin() {
             <Button.Primary type="submit">Entrar</Button.Primary>
           </form>
 
-          <footer className="w-full max-w-sm flex flex-col gap-4">
+          <footer className="flex w-full max-w-sm flex-col gap-4">
             <Separator.Text text="ou" className="mt-3" />
-            <Button.Secondary onClick={loginGoogle} className="w-full max-w-sm flex justify-center items-center gap-4 h-12">
+            <Button.Secondary onClick={loginGoogle} className="flex h-12 w-full max-w-sm items-center justify-center gap-4">
               <GoogleLogo size={32} weight="fill" />
               Entrar com o Google
             </Button.Secondary>
