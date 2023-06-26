55808834754b38e931f479203b961f645aeb063c
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2023-06-26 20:28:22 -0300

    feat(notes): :sparkles: implementa chamada obter as anotações do usuário

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -5,7 +5,7 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, User } from "firebase/aut
 import { useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { app } from "../services/firebaseConfig";
 import { ROUTES } from "../helpers/routes";
-import { collection, doc, getFirestore, setDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getFirestore, setDoc, updateDoc } from "firebase/firestore";
 
 interface AuthContextProps {
   signed: boolean;
@@ -14,15 +14,30 @@ interface AuthContextProps {
   signinGoogle: () => void;
   signup: (email: string, password: string) => void;
   logout: () => JSX.Element;
+  fetchUserData: () => void;
+  notes: Note[];
 }
 
 interface AuthProviderProps {
   children: ReactNode;
 }
 
+interface UserData {
+  email: string;
+  userId: string;
+  notes: Note[];
+}
+
+export interface Note {
+  title: string;
+  categories: string[];
+  note: string;
+  date?: string;
+}
+
 const provider = new GoogleAuthProvider();
 
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
 export const AuthContext = createContext<AuthContextProps>({
   signed: false,
@@ -38,7 +53,9 @@ export const AuthContext = createContext<AuthContextProps>({
       throw new Error("Não foi possível processar sua solicitação");
     }
   },
-  logout: () => <Navigate to={ROUTES.BASE} />
+  logout: () => <Navigate to={ROUTES.BASE} />,
+  fetchUserData: () => {},
+  notes: []
 });
 
 export const AuthGoogleProvider = ({ children }: AuthProviderProps) => {
@@ -48,11 +65,26 @@ export const AuthGoogleProvider = ({ children }: AuthProviderProps) => {
   const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
 
   const [loggedUser, setLoggedUser] = useState<User | null | string>(null);
+  const [notes, setNotes] = useState<Note[]>([]);
 
   const addUserIndDatabase = async (uid: string, userData: { email: string }) => {
     const userCollectionRef = doc(collection(db, "users"), uid);
     await setDoc(userCollectionRef, userData);
   };
+
+  async function addUserIndDatabaseByGoogle(userID: string, userData: UserData) {
+    const userDocRef = doc(db, "users", userID);
+    const userDocSnapshot = await getDoc(userDocRef);
+
+    if (userDocSnapshot.exists()) {
+      const existingUserData = userDocSnapshot.data() as UserData;
+      const mergedNotes = [...existingUserData.notes, ...userData.notes];
+
+      await updateDoc(userDocRef, { notes: mergedNotes });
+    } else {
+      await setDoc(userDocRef, userData);
+    }
+  }
 
   function signin(email: string, password: string) {
     signInWithEmailAndPassword(email, password)
@@ -63,6 +95,7 @@ export const AuthGoogleProvider = ({ children }: AuthProviderProps) => {
             setLoggedUser(userCredential.user);
             sessionStorage.setItem("@AuthFirebase:token", accessToken);
             sessionStorage.setItem("@AuthFirebase:user", JSON.stringify(userCredential?.user));
+            sessionStorage.setItem("@AuthFirebase:userID", userCredential.user.uid);
           })
           .catch((error) => {
             throw new Error("Erro ao obter accessToken:", error);
@@ -79,11 +112,12 @@ export const AuthGoogleProvider = ({ children }: AuthProviderProps) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential?.accessToken ?? "";
         const user = result.user;
-        const userData = { email: user.email ?? "" };
-        addUserIndDatabase(user.uid, userData);
+        const userData = { email: user.email ?? "", userId: user.uid, notes: [] };
+        addUserIndDatabaseByGoogle(user.uid, userData);
         setLoggedUser(user);
         sessionStorage.setItem("@AuthFirebase:token", token);
         sessionStorage.setItem("@AuthFirebase:user", JSON.stringify(user));
+        sessionStorage.setItem("@AuthFirebase:userID", user.uid);
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -96,7 +130,7 @@ export const AuthGoogleProvider = ({ children }: AuthProviderProps) => {
     createUserWithEmailAndPassword(email, password)
       .then((data) => {
         const uid = data?.user.uid ?? "";
-        const userData = { email };
+        const userData = { email, userId: data?.user.uid };
         addUserIndDatabase(uid, userData);
         data?.user
           .getIdToken()
@@ -104,6 +138,7 @@ export const AuthGoogleProvider = ({ children }: AuthProviderProps) => {
             setLoggedUser(data.user);
             sessionStorage.setItem("@AuthFirebase:token", accessToken);
             sessionStorage.setItem("@AuthFirebase:user", JSON.stringify(data?.user));
+            sessionStorage.setItem("@AuthFirebase:userID", data.user.uid);
           })
           .catch((error) => {
             throw new Error("Erro ao obter accessToken:", error);
@@ -119,6 +154,19 @@ export const AuthGoogleProvider = ({ children }: AuthProviderProps) => {
     setLoggedUser(null);
     return <Navigate to={ROUTES.BASE} />;
   }
+
+  const fetchUserData = async () => {
+    const userId = sessionStorage.getItem("@AuthFirebase:userID") ?? "";
+    const userRef = doc(collection(db, "users"), userId);
+    const userDoc = await getDoc(userRef);
+
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+      setNotes(userData.notes);
+    } else {
+      throw new Error("Documento do usuário não encontrado");
+    }
+  };
 
   useEffect(() => {
     const checkUserData = () => {
@@ -141,7 +189,9 @@ export const AuthGoogleProvider = ({ children }: AuthProviderProps) => {
         signin,
         signinGoogle,
         signup,
-        logout
+        logout,
+        fetchUserData,
+        notes: notes
       }}
     >
       {children}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { ROUTES } from "../../helpers/routes";
 import { Sidebar } from "../../components/Sidebar";
@@ -6,16 +6,35 @@ import { AuthContext } from "../../contexts/auth";
 import { Notes } from "../Notes";
 
 export function Home() {
-  const { logout } = useContext(AuthContext);
+  const { fetchUserData, logout } = useContext(AuthContext);
   const { pathname } = useLocation();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const storageUser = sessionStorage.getItem("@AuthFirebase:user");
   const userName = storageUser && JSON.parse(storageUser);
 
+  const getInfos = () => {
+    setIsLoading(true);
+    fetchUserData();
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getInfos();
+  }, []);
+
   return (
-    <div className="flex h-screen w-screen">
-      <Sidebar username={userName.displayName} handleExit={logout} />
-      <div className="w-full bg-gray-400  p-8">{pathname === ROUTES.HOME ? <Notes /> : <Outlet />}</div>
-    </div>
+    <>
+      {isLoading ? (
+        // TODO: adicionar loading
+        <p>Carregando...</p>
+      ) : (
+        <div className="flex h-screen w-screen">
+          <Sidebar username={userName.displayName} handleExit={logout} />
+          <div className="w-full bg-gray-400  p-8">{pathname === ROUTES.HOME ? <Notes /> : <Outlet />}</div>
+        </div>
+      )}
+    </>
   );
 }
